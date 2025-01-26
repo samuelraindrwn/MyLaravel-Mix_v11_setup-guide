9196a31148ba1ecb9824ebe855c51e667772a7c4
Samuel Rai <samuelraycontact1@gmail.com>
2025-01-27 02:36:25 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'showGreeting'])->name('index');
 
-
-// Route::get('/', function () {
-//     return view('pages/user/home');
-// })->name('index');
-
 Route::get('/dashboard', function () {
     return view('pages/admin/dashboard');
 })->name('admin-dashboard');
